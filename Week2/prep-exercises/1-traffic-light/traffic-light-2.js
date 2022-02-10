67bbd75eef73b4cfda393ce0b1c66b9ecf931ede
cometbroom <alawi.wow@gmail.com>
2022-02-10 15:52:02 +0100

    traffic light week 2

--- Week2/prep-exercises/1-traffic-light/traffic-light-2.js
+++ Week2/prep-exercises/1-traffic-light/traffic-light-2.js
@@ -5,19 +5,26 @@
  * The `stateIndex` property indicates which of the possible states is current.
  */
 const trafficLight = {
-  possibleStates: ["green", "orange", "red"],
-  stateIndex: 0,
+    possibleStates: ["green", "orange", "red"],
+    stateIndex: 0,
 };
 
 let cycle = 0;
 while (cycle < 2) {
-  const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
-  console.log("The traffic light is on", currentState);
+    const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
+    console.log("The traffic light is on", currentState);
 
-  // TODO
-  // if the color is green, turn it orange
-  // if the color is orange, turn it red
-  // if the color is red, add 1 to cycles and turn it green
+    if (currentState === "green") trafficLight.stateIndex = 1;
+    if (currentState === "orange") trafficLight.stateIndex = 2;
+    if (currentState === "red") {
+        trafficLight.stateIndex = 0;
+        cycle++;
+    }
+
+    // TODO
+    // if the color is green, turn it orange
+    // if the color is orange, turn it red
+    // if the color is red, add 1 to cycles and turn it green
 }
 
 /**
